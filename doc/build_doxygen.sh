0dfe67504c2c9cf250dc514672856a6f79437b85
Baek,Minki <vateran.net@gmail.com>
2017-06-07 23:51:01 +0900

    essential

---
+++ doc/build_doxygen.sh
@@ -0,0 +1,3 @@
+cd ..
+/Applications/Doxygen.app/Contents/Resources/doxygen ./doc/todengine3.doxygen
+cd doc
---
+++ external/wxWidgets/build/osx/bin/ios/intermediates/wxcocoa.build/Debug/wxtiff.build/Script-94E713C89B403F568A694FAE.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+"${WXROOT}/distrib/mac/pbsetup-sh" "${WXROOT}/src" "${WXROOT}/build/osx/setup/${WXTOOLKIT}"
